88e7906cb9e2097e156914cd39150da68cee7ae2
Pratham460 <61970135+Pratham460@users.noreply.github.com>
2020-03-13 20:47:29 +0530

    Add files via upload

--- tk.py
+++ tk.py
@@ -41,15 +41,14 @@ def checkC():
        messagebox.showinfo('Message','You Loose\nThe number was '+str(picked))
        window.destroy()
 
-    if int(text_input) not in numbers:
-       messagebox.showinfo('Message','Number does not exist in the list')
+    
       
         
 import random
 
 
-numbers=[11,12,13,14,15]
-picked=random.choice(numbers)
+
+picked=random.randrange(0,99)
         
 
 import tkinter as tk
@@ -59,56 +58,59 @@ from tkinter import messagebox
 window=tk.Tk()
 window.configure(bg="orange")
 
-window.geometry("500x800")
+window.geometry("600x500")
 op=" "
-chances=3
-print(picked)
+chances=7
+
 
 
 b0=tk.Button(window,bd=5,text="0",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(0))
-b0.grid(row=4,column=2)
+b0.grid(row=5,column=2)
         
 b1=tk.Button(window,bd=5,text="1",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(1))
-b1.grid(row=1,column=1)
+b1.grid(row=2,column=1)
 
 b2=tk.Button(window,bd=5,text="2",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(2))
-b2.grid(row=1,column=2)
+b2.grid(row=2,column=2)
 
 b3=tk.Button(window,bd=5,text="3",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(3))
-b3.grid(row=1,column=3)
+b3.grid(row=2,column=3)
 
 b4=tk.Button(window,bd=5,text="4",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(4))
-b4.grid(row=2,column=1)
+b4.grid(row=3,column=1)
 
 b5=tk.Button(window,bd=5,text="5",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(5))
-b5.grid(row=2,column=2)
+b5.grid(row=3,column=2)
 
 b6=tk.Button(window,bd=5,text="6",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(6))
-b6.grid(row=2,column=3)
+b6.grid(row=3,column=3)
 
 b7=tk.Button(window,bd=5,text="7",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(7))
-b7.grid(row=3,column=1)
+b7.grid(row=4,column=1)
 
 b8=tk.Button(window,bd=5,text="8",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(8))
-b8.grid(row=3,column=2)
+b8.grid(row=4,column=2)
 
 b9=tk.Button(window,bd=5,text="9",height=3,width=7,bg="light blue",fg="red",command=lambda:btnClick(9))
-b9.grid(row=3,column=3)
+b9.grid(row=4,column=3)
 
 text_input=tk.StringVar()
 txtd=tk.Entry(bd=4,font=("arial",18,"bold"),textvariable=text_input,bg="white")
-txtd.grid(row=0,column=0)
+txtd.grid(row=1,column=0)
 
 
 
 b11=tk.Button(window,bd=8,text="Submit",bg="light green",fg="red",font=("arial",12),height=3,width=10,command=checkC)
-b11.grid(row=4,column=0)
+b11.grid(row=6,column=0)
 
 bc=tk.Button(bd=8,text="C",bg="light green",fg="red",font=("arial",12,"bold"),width=8,height=2,command=btnC)
-bc.grid(row=5,column=0)
+bc.grid(row=7,column=0)
 
-label=tk.Label(window,text="Total chances are: 3",fg="purple",font=("arial",17))
+label=tk.Label(window,text="Total chances are: 7",fg="purple",font=("arial",17))
 label.grid(row=8,column=0)
+
+label=tk.Label(window,text="Guess the number",fg="purple",font=("arial",17))
+label.grid(row=0,column=0)
 
 
 
